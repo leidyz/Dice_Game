547e8f90cc690d9f8c750fed52af4ec8f44c4927
leidyz <leidy.zhang98@gmail.com>
2023-11-30 16:12:19 +0100

    created user model and controller

--- DiceGame/app/Models/User.php
+++ DiceGame/app/Models/User.php
@@ -18,19 +18,19 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
-     *
      * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'role'
     ];
 
     /**
