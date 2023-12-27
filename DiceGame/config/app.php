8b507414e4286ec358ae837aebda20c9a449f903
leidyz <leidy.zhang98@gmail.com>
2023-12-27 14:55:22 -0500

    passport installation

--- DiceGame/config/app.php
+++ DiceGame/config/app.php
@@ -169,11 +169,13 @@ return [
         /*
          * Application Service Providers...
          */
+        Laravel\Passport\PassportServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
     ])->toArray(),
 
     /*
