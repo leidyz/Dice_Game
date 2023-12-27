715eb109e4839bf5b9f91eeb0418740f1acf4f8b
leidyz <leidy.zhang98@gmail.com>
2023-12-27 16:43:39 -0500

    registration: 3 api routes

--- DiceGame/app/Http/Controllers/UserController.php
+++ DiceGame/app/Http/Controllers/UserController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller; 
+use App\Models\User; 
+use Illuminate\Support\Facades\Auth; 
+use Validator;
 
 class UserController extends Controller
 {
-    //
+    public $successStatus = 200;
+    public $createdStatus = 201;
+    public $unauthorizedStatus = 401;
+    public $notFoundStatus = 404;
+
+    public function register(Request $request){
+
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function logout(Request $request){
+        
+    }
 }
