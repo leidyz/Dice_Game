9c2576d487095965fd5dd1bcaa4a855d7decbf4b
leidyz <leidy.zhang98@gmail.com>
2024-01-10 13:43:27 +0100

    game controller: separate create logic

--- DiceGame/app/Http/Controllers/GameController.php
+++ DiceGame/app/Http/Controllers/GameController.php
@@ -23,52 +23,42 @@ class GameController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function play()
-    {
-        //Route::post('/players/{id}/games/','play')->name('games.play');
+    public function createGame($user){
 
-        //authenticate the user
-        $user = Auth::guard('api')->user();
-        $user = Auth::user();
-        if (!$user) {
-            return response()->json(['message' => 'Unauthorized'], 401);
-        }
-
-        //game settings
-        $dice1 = rand(1,6);
-        $dice2 = rand(1,6);
+        $dice1 = random_int(1,6);
+        $dice2 = random_int(1,6);
         $isWon = false;
+
         if($dice1 + $dice2 == 7){
             $isWon = true;
         }
         
-        $game = Game::create([
+        return Game::create([
             'dice1' => $dice1,
             'dice2'=> $dice2,
             'isWon'=> $isWon,
             'user_id'=> $user->id,
         ]);
+    }
+    public function play($user){
 
-        $winningMessage = "Congratulations, you won!";
-        $losingMessage = "Sorry, you lost!";
-
-        if($isWon == true){
-            $message = $winningMessage;
-        }else{
-            $message = $losingMessage;
+        $user = Auth::guard('api')->user();
+        if (!$user) {
+            return response()->json(['message' => 'Unauthorized'], 401);
         }
+
+       $game = $this->createGame($user);
 
         return response()->json([
             'welcome' => 'Welcome to the Dice Game!',
-            'dice1'=> 'Your first dice is '.$dice1,
-            'dice2'=> 'Your second dice is '.$dice2,
-            'total'=> 'The total of your dices are '. ($dice1+$dice2),
-            'result'=> $message,
+            'dice1'=> 'Your first dice is '.$game->dice1,
+            'dice2'=> 'Your second dice is '.$game->dice2,
+            'total'=> 'The total of your dices are '. ($game->dice1+$game->dice2),
+            'result'=> $game->isWon ? 'Congratulations, you won!':'Sorry, you lost!',
             'status' => 'success',
             'message'=> 'Your game has registered successfully!',
             'game' => $game,
         ]);
-
 
     }
 
--- DiceGame/routes/api.php
+++ DiceGame/routes/api.php
@@ -18,17 +18,17 @@ use Illuminate\Support\Facades\Route;
 */
 // Public routes of authtication
 Route::controller(LoginRegisterController::class)->group(function() {
-    Route::post('/register', 'register');
-    Route::post('/login', 'login');
+    Route::post('/register', 'register')->name('user.register');
+    Route::post('/login', 'login')->name('user.login');
 });
 
 Route::middleware('auth:api')->group( function () {
-    Route::post('/logout', [LoginRegisterController::class, 'logout']);
+    Route::post('/logout', [LoginRegisterController::class, 'logout'])->name('user.logout');
 });
 
-Route::controller(GameController::class)->group(function(){
-    Route::post('/players/{id}/games/','play')->name('games.play');
-    Route::delete('/players/{id}/games/','delete')->name('games.delete');
-    Route::get('players/{id}/games','index')->name('games.index');
+Route::middleware('auth:api')->group(function(){
+    Route::post('/players/{id}/games/',[GameController::class,'play'])->name('games.play');
+    Route::delete('/players/{id}/games/',[GameController::class,'delete'])->name('games.delete');
+    Route::get('players/{id}/games',[GameController::class,'index'])->name('games.index');
 });
 
