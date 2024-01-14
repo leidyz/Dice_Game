7e75a78307b8594226408bbcebb5b368707c10f7
leidyz <leidy.zhang98@gmail.com>
2024-01-14 20:02:56 +0100

    gameTest: play test

--- DiceGame/tests/Feature/GameTest.php
+++ DiceGame/tests/Feature/GameTest.php
@@ -4,17 +4,38 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Game;
 
 class GameTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function setUp(): void {
+        parent::setUp();
+        Artisan::call('db:seed', ['--class' => 'RoleSeeder']);
     }
+    public function testPlay()
+    {
+        $user = User::factory()->create();
+        $user->assignRole('player');
+        $this->actingAs($user, 'api');
+
+        $response = $this->json('POST', '/api/players/' . $user->id . '/games');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'welcome',
+                'dice1',
+                'dice2',
+                'total',
+                'result',
+                'status',
+                'message',
+                'game' => ['id', 'dice1', 'dice2', 'isWon', 'user_id', 'created_at', 'updated_at'],
+            ]);
+    }
+
 }
