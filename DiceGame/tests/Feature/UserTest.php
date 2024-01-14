e7af8034cc37d141a47ac0afcb0247c0e8155607
leidyz <leidy.zhang98@gmail.com>
2024-01-14 19:43:31 +0100

    usertest: roles test

--- DiceGame/tests/Feature/UserTest.php
+++ DiceGame/tests/Feature/UserTest.php
@@ -12,17 +12,14 @@ class UserTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    public function setUp(): void
-    {
+    public function setUp(): void{
         parent::setUp();
         Artisan::call('db:seed', ['--class' => 'RoleSeeder']);
         Artisan::call('passport:install');
     }
 
 
-    public function testUpdateNameSuccessfully()
-    {
-       
+    public function testUpdateNameSuccessfully(){
         $user = User::factory()->create();
         $user->assignRole('player'); 
 
@@ -34,6 +31,68 @@ class UserTest extends TestCase
             ]);
 
         $this->assertDatabaseHas('users', ['id' => $user->id, 'name' => 'NewName']);
+    }
+
+    public function testUpdateNameWithValidationErrors(){
+    
+        $user = User::factory()->create();
+        $user->assignRole('player'); 
+        $existingUser = User::factory()->create(['name' => 'ExistingName']);
+        $response = $this->actingAs($user, 'api')->putJson('/api/players/' . $user->id, ['newName' => 'ExistingName']);
+
+        $response->assertStatus(422)
+            ->assertJsonStructure([
+                'error' => [
+                    'newName',
+                ],
+            ]);
+    }
+
+
+    public function testIndex() {
+        $user = User::factory()->create();
+        $user->assignRole('admin'); 
+        $response = $this->actingAs($user, 'api')->getJson('/api/players/');
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['Players Average Success Rate', 'users']);
+    }
+
+    public function testGetRanking() { 
+        $user = User::factory()->create();
+        $user->assignRole('admin'); 
+        $response = $this->actingAs($user, 'api')->getJson('/api/players/ranking');
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['Players Ranking']);
+    }
+
+    public function testGetLoser(){
+        $user = User::factory()->create();
+        $user->assignRole('admin');
+        $response = $this->actingAs($user,'api')->getJson('/api/players/ranking/loser');
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['Worst Success Rate']);
+    }
+
+    public function testGetWinner(){
+        $user = User::factory()->create();
+        $user->assignRole('admin');
+        $response = $this->actingAs($user,'api')->getJson('/api/players/ranking/winner');
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['Best Success Rate']);
+    }
+
+    public function testPlayerCannotAccessAdminMethods() {
+        $user = User::factory()->create();
+        $user->assignRole('player');
+        $response = $this->actingAs($user, 'api')->getJson('/api/players/ranking/loser');
+        $response->assertStatus(403);
+    }
+
+    public function testAdminCannotAccessPlayerMethods(){
+        $user = User::factory()->create();
+        $user->assignRole('admin');
+        $response = $this->actingAs($user,'api')->getJson('/api/players/{$user->id}/games/');
+        $response->assertStatus(403);
     }
 
 }
