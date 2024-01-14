7ccf554e4dc2ad1b41d1cdb7df37ebb6ac75279b
leidyz <leidy.zhang98@gmail.com>
2024-01-14 17:50:25 +0100

    usertest: registration test

--- DiceGame/tests/Feature/UserTest.php
+++ DiceGame/tests/Feature/UserTest.php
@@ -5,16 +5,43 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    public function testUserRegistration()
+    {
+        $userData = [
+            'name' => 'karly',
+            'email' => 'karly@example.com',
+            'password' => 'asdfg123',
+            'password_confirmation' => 'asdfg123',
+        ];
+
+        $response = $this->postJson('/api/players', $userData);
+
+        $response->assertJson([
+            'status' => 'success',
+            'message' => 'User is created successfully.',
+            'data' => $userData,
+        ]);
+        $response->assertStatus(201);
+    }
+
+    public function testUserRegistrationWithValidationErrors()
+    {
+        // Missing email
+        $invalidUserData = [
+            'name' => 'karly',
+            'password' => 'asdfg123',
+            'password_confirmation' => 'asdfg123',
+        ];
+
+        $response = $this->postJson('/api/players', $invalidUserData);
+
+        $response->assertStatus(403);
+        $response->assertJsonStructure(['status', 'message', 'data']);
+        $response->assertJsonValidationErrors(['email']);
     }
 }
